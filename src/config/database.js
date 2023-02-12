a51b47f8db82a192f651b987c70a9223b453bba2
duanzin <lucaduan@gmail.com>
2023-02-12 17:09:40 -0300

    fix: erro de digitação em database.js corrigido

--- src/config/database.js
+++ src/config/database.js
@@ -4,10 +4,10 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const connection = new Pool({
+const configDatabase = {
   connectionString: process.env.DATABASE_URL,
-});
+};
 
 if (process.env.MODE === "prod") configDatabase.ssl = true;
 
-export const db = new Pool(connection);
+export const db = new Pool(configDatabase);

ea10b5017fd5b56b387110a50508a071705a1c63
duanzin <lucaduan@gmail.com>
2023-02-12 18:26:49 -0300

    fix: correção na criação de games

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -13,18 +13,18 @@ export async function createGame(req, res) {
   const { name, image, stockTotal, pricePerDay } = req.body;
 
   try {
-    const gameExists = await db.query(`SELECT * FROM games WHERE name = $1`, [
+    const gameExiste = await db.query(`SELECT * FROM games WHERE name = $1`, [
       name,
     ]);
-    if (gameExists.rows.length !== 0) {
+    if (gameExiste.rows.length !== 0) {
       return res.sendStatus(409);
     }
     await db.query(
       `INSERT INTO games (name,image,stockTotal,pricePerDay) VALUES ($1,$2,$3,$4)`,
       [name, image, stockTotal, pricePerDay]
     );
-    res.sendStatus(201);
+    return res.sendStatus(201);
   } catch (err) {
-    res.status(500).send(err.message);
+    return res.status(500).send(err.message);
   }
 }

dae5907a7ac9c2840cd858bcd9eaf7ae7374daa1
duanzin <lucaduan@gmail.com>
2023-02-13 11:53:36 -0300

    fix: correção na busca por dados de um customer por id

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -12,13 +12,15 @@ export async function showCustomers(req, res) {
 export async function showCustomer(req, res) {
   const { id } = req.params;
   try {
-    const customer = await db.query(`SELECT * FROM customers WHERE id=$1`, [
+    const customer = await db.query(`SELECT * FROM customers WHERE id = $1`, [
       id,
     ]);
-    if (customer.rows.length == 0) return res.sendStatus(404);
-    res.send(customer.rows[0]);
-  } catch (error) {
-    res.status(500).send(error.message);
+    if (!customer.rows[0]) {
+      return res.sendStatus(404);
+    }
+    res.status(200).send(customer.rows[0]);
+  } catch (err) {
+    res.status(500).send(err.message);
   }
 }
 
